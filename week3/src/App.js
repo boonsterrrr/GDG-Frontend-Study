9bea6c8efc99a2f273b66894ec8e8ea1112f135c
정지현 <boonsterrrr@gmail.com>
2025-11-05 16:31:17 +0900

    과제

--- week3/src/App.js
+++ week3/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Dog from "./family/Dog";
+
+import Father from "./family/Father";
+
+import Me from "./family/Me";
+
+import ME from "./ME";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Me />
+
+      <Father />
+
+      <Dog />
+      <ME />
     </div>
   );
 }
